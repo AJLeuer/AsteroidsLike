9f554b88204de6e61794a054243bc55f19fe4d71
Adam J. Leuer <Adam@host-15-169.iluruco.clients.pavlovmedia.com>
2014-03-18 19:14:15 -0500

    Retrying adding files

---
+++ Adapter/Adapter.cpp
@@ -0,0 +1,9 @@
+//
+//  Adapter.cpp
+//  GameWorld
+//
+//  Created by Adam James Leuer on 3/18/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#include "Adapter.h"
---
+++ Adapter/Adapter.h
@@ -0,0 +1,14 @@
+//
+//  Adapter.h
+//  GameWorld
+//
+//  Created by Adam James Leuer on 3/18/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#ifndef __GameWorld__Adapter__
+#define __GameWorld__Adapter__
+
+#include <iostream>
+
+#endif /* defined(__GameWorld__Adapter__) */
