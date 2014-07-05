f911b655f3b3ffe52fe66a7de01d9016c2519528
Adam J. Leuer <Adam@host-15-234.iluruco.clients.pavlovmedia.com>
2014-07-05 13:37:20 -0500

    Created TextOutput class

---
+++ Output/TextOutput.cpp
@@ -0,0 +1,9 @@
+//
+//  TextOutput.cpp
+//  SpriteFight
+//
+//  Created by Adam James Leuer on 7/5/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#include "TextOutput.h"
---
+++ Output/TextOutput.h
@@ -0,0 +1,21 @@
+//
+//  TextOutput.h
+//  SpriteFight
+//
+//  Created by Adam James Leuer on 7/5/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#ifndef __SpriteFight__TextOutput__
+#define __SpriteFight__TextOutput__
+
+#include <iostream>
+#include <SDL_ttf/SDL_ttf.h>
+
+#include "GraphicalOutput.h"
+
+class TextOutput : public GraphicalOutput {
+	
+} ;
+
+#endif /* defined(__SpriteFight__TextOutput__) */
